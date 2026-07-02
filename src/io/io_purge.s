1d2f2ca6d9f3cc00bf8884c7bb20811aa78c1c49
0plimplim0 <cesarcortes5to@gmail.com>
2026-07-02 17:35:18 -0600

    Add io_purge for io_utils

--- src/io/io_purge.s
+++ src/io/io_purge.s
@@ -1,32 +1,14 @@
 .intel_syntax noprefix
 
-# TODO: Update this to IO_STREAM format
-# NEW: io_purge(IO_STREAM)
-# Just set IO_STREAM counters to 0
-
 .global io_purge
 io_purge:
-  # io_purge(rdi = fd)
+  # io_purge(rdi = IO_STREAM)
   # Prologue
-  push r12
   push rbp
   mov rbp, rsp
-  
-  mov r12, rdi
-  sub rsp, 256
-.loop:
-  mov eax, 0
-  mov rdi, r12
-  mov rsi, rsp
-  mov edx, 256
-  syscall
-  cmp eax, 0
-  jbe .prologue
-  cmp byte ptr [rsp+rax-1], 0x0A
-  je .prologue
-  jmp .loop
-.prologue:
+
+  mov qword ptr [rdi+12], 0
+  #Epilogue
   mov rsp, rbp
   pop rbp
-  pop r12
   ret
